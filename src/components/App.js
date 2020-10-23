8bbfc45985904c8eeede7733824cd88a8a081e6c
Pedro Medina <pedromedina190@gmail.com>
2020-10-23 09:13:45 -0300

    Setup routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router";
 import CreateLink from "./CreateLink";
+import Header from "./Header";
 import LinkList from "./LinkList";
 
 function App() {
   return (
-    <>
-      <br />
-      <LinkList />
-      <br />
-      <hr />
-      <br />
-      <CreateLink />
-    </>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
